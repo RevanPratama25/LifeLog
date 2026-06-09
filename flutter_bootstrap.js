f8489e4aa354056b7f2a109ed703cabc6b781881
RevanPratama25 <RevanPratama25@users.noreply.github.com>
2026-06-09 00:40:27 +0000

    deploy: d99b836fce4e3cbd97d564ae536e9e4c0cba8cac

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3220748987" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2392150489" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
